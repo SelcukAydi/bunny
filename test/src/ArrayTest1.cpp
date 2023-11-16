588a9fc371bd6cf6c901a07fd7772b76e373de2b
selcuk <slck.aydi@gmail.com>
2023-11-16 18:25:48 +0000

    Update

--- test/src/ArrayTest1.cpp
+++ test/src/ArrayTest1.cpp
@@ -1,98 +1,98 @@
-#include "ArrayTest1.hpp"
-#include <gtest/gtest.h>
+// #include "ArrayTest1.hpp"
+// #include <gtest/gtest.h>
 
 
-TEST(ArrayTest1, Simple)
-{
-    SimpleTestClass out_obj;
-    std::stringstream ss;
-    bunny::ComposerPaper composer{ss};
+// TEST(ArrayTest1, Simple)
+// {
+//     SimpleTestClass out_obj;
+//     std::stringstream ss;
+//     bunny::ComposerPaper composer{ss};
 
-    out_obj.m_arr[0] = 100;
-    out_obj.m_arr[1] = 101;
-    out_obj.m_arr[2] = 102;
-    out_obj.m_arr[3] = 103;
-    out_obj.m_arr[4] = 104;
+//     out_obj.m_arr[0] = 100;
+//     out_obj.m_arr[1] = 101;
+//     out_obj.m_arr[2] = 102;
+//     out_obj.m_arr[3] = 103;
+//     out_obj.m_arr[4] = 104;
 
-    composer << out_obj;
+//     composer << out_obj;
 
-    bunny::DecomposerPaper decomposer{ss};
-    SimpleTestClass in_obj;
-    decomposer >> in_obj;
+//     bunny::DecomposerPaper decomposer{ss};
+//     SimpleTestClass in_obj;
+//     decomposer >> in_obj;
 
-    EXPECT_TRUE(in_obj == out_obj);
-}
+//     EXPECT_TRUE(in_obj == out_obj);
+// }
 
-TEST(ArrayTest1, IgnoredKey)
-{
-    KeyIgnoreTestClass out_obj;
-    std::stringstream ss;
-    bunny::ComposerPaper composer{ss};
+// TEST(ArrayTest1, IgnoredKey)
+// {
+//     KeyIgnoreTestClass out_obj;
+//     std::stringstream ss;
+//     bunny::ComposerPaper composer{ss};
 
-    out_obj.m_arr[0] = 100;
-    out_obj.m_arr[1] = 101;
-    out_obj.m_arr[2] = 102;
-    out_obj.m_arr[3] = 103;
-    out_obj.m_arr[4] = 104;
+//     out_obj.m_arr[0] = 100;
+//     out_obj.m_arr[1] = 101;
+//     out_obj.m_arr[2] = 102;
+//     out_obj.m_arr[3] = 103;
+//     out_obj.m_arr[4] = 104;
 
-    composer << out_obj;
+//     composer << out_obj;
 
-    bunny::DecomposerPaper decomposer{ss};
-    KeyIgnoreTestClass in_obj;
-    decomposer >> in_obj;
+//     bunny::DecomposerPaper decomposer{ss};
+//     KeyIgnoreTestClass in_obj;
+//     decomposer >> in_obj;
 
-    EXPECT_FALSE(in_obj == out_obj);
-}
+//     EXPECT_FALSE(in_obj == out_obj);
+// }
 
-TEST(ArrayTest1, CascadeArrayTest)
-{
-    CascadeArrayTestClass out_obj;
-    std::stringstream ss;
-    bunny::ComposerPaper composer{ss};
+// TEST(ArrayTest1, CascadeArrayTest)
+// {
+//     CascadeArrayTestClass out_obj;
+//     std::stringstream ss;
+//     bunny::ComposerPaper composer{ss};
 
-    SimpleTestClass test1;
-    test1.m_arr[0] = 100;
-    test1.m_arr[1] = 101;
-    test1.m_arr[2] = 102;
-    test1.m_arr[3] = 103;
-    test1.m_arr[4] = 104;
+//     SimpleTestClass test1;
+//     test1.m_arr[0] = 100;
+//     test1.m_arr[1] = 101;
+//     test1.m_arr[2] = 102;
+//     test1.m_arr[3] = 103;
+//     test1.m_arr[4] = 104;
 
-    SimpleTestClass test2;
-    test2.m_arr[0] = 900;
-    test2.m_arr[1] = 901;
-    test2.m_arr[2] = 902;
-    test2.m_arr[3] = 903;
-    test2.m_arr[4] = 904;
+//     SimpleTestClass test2;
+//     test2.m_arr[0] = 900;
+//     test2.m_arr[1] = 901;
+//     test2.m_arr[2] = 902;
+//     test2.m_arr[3] = 903;
+//     test2.m_arr[4] = 904;
 
-    out_obj.m_class_list[0] = test1;
-    out_obj.m_class_list[9] = test2;
+//     out_obj.m_class_list[0] = test1;
+//     out_obj.m_class_list[9] = test2;
 
-    composer << out_obj;
+//     composer << out_obj;
 
-    bunny::DecomposerPaper decomposer{ss};
-    CascadeArrayTestClass in_obj;
-    decomposer >> in_obj;
+//     bunny::DecomposerPaper decomposer{ss};
+//     CascadeArrayTestClass in_obj;
+//     decomposer >> in_obj;
 
-    EXPECT_TRUE(in_obj == out_obj);
-}
+//     EXPECT_TRUE(in_obj == out_obj);
+// }
 
-TEST(ArrayTest1, SimpleStringTest)
-{
-    SimpleStringTestClass out_obj;
-    std::stringstream ss;
-    bunny::ComposerPaper composer{ss};
+// TEST(ArrayTest1, SimpleStringTest)
+// {
+//     SimpleStringTestClass out_obj;
+//     std::stringstream ss;
+//     bunny::ComposerPaper composer{ss};
 
-    out_obj.m_arr[0] = "a";
-    out_obj.m_arr[0] = "b";
-    out_obj.m_arr[0] = "c";
-    out_obj.m_arr[0] = "d";
-    out_obj.m_arr[0] = "e";
+//     out_obj.m_arr[0] = "a";
+//     out_obj.m_arr[0] = "b";
+//     out_obj.m_arr[0] = "c";
+//     out_obj.m_arr[0] = "d";
+//     out_obj.m_arr[0] = "e";
 
-    composer << out_obj;
+//     composer << out_obj;
 
-    bunny::DecomposerPaper decomposer{ss};
-    SimpleStringTestClass in_obj;
-    decomposer >> in_obj;
+//     bunny::DecomposerPaper decomposer{ss};
+//     SimpleStringTestClass in_obj;
+//     decomposer >> in_obj;
 
-    EXPECT_TRUE(in_obj == out_obj);
-}
+//     EXPECT_TRUE(in_obj == out_obj);
+// }
--- test/src/SharedPtrTest1.cpp
+++ test/src/SharedPtrTest1.cpp
@@ -1,71 +1,71 @@
-#include "SharedPtrTest1.hpp"
-#include "SharedPointer.hpp"
+// #include "SharedPtrTest1.hpp"
+// #include "SharedPointer.hpp"
 
-#include <memory>
-#include <gtest/gtest.h>
+// #include <memory>
+// #include <gtest/gtest.h>
 
-TEST(SharedptrTest1, Simple)
-{
-    std::stringstream ss;
-    bunny::ComposerPaper composer{ss};
+// TEST(SharedptrTest1, Simple)
+// {
+//     std::stringstream ss;
+//     bunny::ComposerPaper composer{ss};
 
-    SharedPtrSimpleTestClass out_obj;
-    out_obj.m_id = 100;
-    out_obj.m_name = "sia";
+//     SharedPtrSimpleTestClass out_obj;
+//     out_obj.m_id = 100;
+//     out_obj.m_name = "sia";
 
-    auto ptr = std::make_shared<SharedPtrPersonTestClass>();
-    ptr->m_number = 200;
+//     auto ptr = std::make_shared<SharedPtrPersonTestClass>();
+//     ptr->m_number = 200;
 
-    out_obj.m_person = ptr;
+//     out_obj.m_person = ptr;
 
-    composer << out_obj;
+//     composer << out_obj;
 
-    bunny::DecomposerPaper decomposer{ss};
-    SharedPtrSimpleTestClass in_obj;
-    decomposer >> in_obj;
+//     bunny::DecomposerPaper decomposer{ss};
+//     SharedPtrSimpleTestClass in_obj;
+//     decomposer >> in_obj;
 
-    EXPECT_TRUE(out_obj.m_person->m_number == in_obj.m_person->m_number);
-}
+//     EXPECT_TRUE(out_obj.m_person->m_number == in_obj.m_person->m_number);
+// }
 
-TEST(SharedptrTest1, Nullptr)
-{
-    std::stringstream ss;
-    bunny::ComposerPaper composer{ss};
+// TEST(SharedptrTest1, Nullptr)
+// {
+//     std::stringstream ss;
+//     bunny::ComposerPaper composer{ss};
 
-    SharedPtrSimpleTestClass out_obj;
-    out_obj.m_id = 100;
-    out_obj.m_name = "sia";
+//     SharedPtrSimpleTestClass out_obj;
+//     out_obj.m_id = 100;
+//     out_obj.m_name = "sia";
 
-    composer << out_obj;
+//     composer << out_obj;
 
-    bunny::DecomposerPaper decomposer{ss};
-    SharedPtrSimpleTestClass in_obj;
-    decomposer >> in_obj;
+//     bunny::DecomposerPaper decomposer{ss};
+//     SharedPtrSimpleTestClass in_obj;
+//     decomposer >> in_obj;
 
-    EXPECT_TRUE(in_obj.m_person.get() == nullptr);
-}
+//     EXPECT_TRUE(in_obj.m_person.get() == nullptr);
+// }
 
-TEST(SharedptrTest1, Complex1)
-{
-    std::stringstream ss;
-    bunny::ComposerPaper composer{ss};
+// TEST(SharedptrTest1, Complex1)
+// {
+//     std::stringstream ss;
+//     bunny::ComposerPaper composer{ss};
 
-    SharedPtrComplexTestClass out_obj;
-    out_obj.m_id = 100;
-    out_obj.m_name = "sia";
+//     SharedPtrComplexTestClass out_obj;
+//     out_obj.m_id = 100;
+//     out_obj.m_name = "sia";
 
-    auto person2 = std::make_shared<SharedPtrPersonTestClass>();
-    person2->m_number = 200;
+//     auto person2 = std::make_shared<SharedPtrPersonTestClass>();
+//     person2->m_number = 200;
 
-    out_obj.m_person1 = nullptr;
-    out_obj.m_person2 = std::make_shared<std::shared_ptr<SharedPtrPersonTestClass>>(person2);
+//     out_obj.m_person1 = nullptr;
+//     out_obj.m_person2 = std::make_shared<std::shared_ptr<SharedPtrPersonTestClass>>(person2);
 
-    composer << out_obj;
+//     composer << out_obj;
 
-    bunny::DecomposerPaper decomposer{ss};
-    SharedPtrComplexTestClass in_obj;
-    decomposer >> in_obj;
+//     bunny::DecomposerPaper decomposer{ss};
+//     SharedPtrComplexTestClass in_obj;
+//     decomposer >> in_obj;
 
-    EXPECT_TRUE(in_obj.m_person1.get() == nullptr);
-    EXPECT_TRUE(in_obj.m_person2.get()->get()->m_number == out_obj.m_person2.get()->get()->m_number);
-}
+//     EXPECT_TRUE(in_obj.m_person1.get() == nullptr);
+//     EXPECT_TRUE(in_obj.m_person2.get()->get()->m_number == out_obj.m_person2.get()->get()->m_number);
+// }
